fdff478b348521e05d01901a1fe0200ed39198cc
maksim <maksymchybisov@gmail.com>
2018-10-26 15:02:30 +0300

    get password

--- src/DataFixtures/UserFixture.php
+++ src/DataFixtures/UserFixture.php
@@ -4,15 +4,23 @@ namespace App\DataFixtures;
 
 use App\Entity\User;
 use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UserFixture extends BaseFixture
 {
+    private $passwordEncoder;
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
     protected function loadData(ObjectManager $manager)
     {
         $this->createMany(User::class, 10, function(User $user) {
 
             $user->setEmail($this->faker->email);
             $user->setFirstName($this->faker->firstName);
+            $user->setPassword($this->passwordEncoder->encodePassword($user, 'engage'));
 
 
         });
